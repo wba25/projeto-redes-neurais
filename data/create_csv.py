1156ea6780244765d578c4389af1cbde26f92888
Wellington Almeida <wba@cin.ufpe.br>
2018-06-12 14:55:28 -0300

    Cria csv apenas com as 10000 instancias do dataset

--- data/create_csv.py
+++ data/create_csv.py
@@ -1,11 +1,14 @@
 import numpy as np
 import pandas as pd
 import csv
+from functools import reduce
 
 file = open("TRN", 'r')
-data = file.read()
+data = file.readlines()
 
-data = data.replace("\t", ",")
+data = list(map(lambda x: x.replace("\t", ","), data[:10001]))
+
+csv_data = reduce((lambda x, y: x+y), data)
 
 csv_file = open("trn.csv", "w")
-csv_file.write(data)
+csv_file.write(csv_data)
